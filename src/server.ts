03176cb9a433affadc5da38fab3be6d37f02bbe3
Rebecca Manzi <rebeccamanzi@gmail.com>
2021-02-22 19:14:39 -0300

    inicializando o projeto

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express, { request } from 'express';
+
+const app = express();
+
+/**
+ * MÉTODOS HTTP
+ * GET => Busca
+ * POST => Salvar 
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração específica (apenas 1 info)
+ */
+
+app.get('/', (request, response) => {
+  return response.json({ message: "Hello World - NLW04" });
+});
+
+// 1 param => Rota(Recurso API)
+// 1 param => request, response
+
+app.post("/", (request, response) => {
+  return response.json({ message: "Os dados foram salvos com sucesso!"});
+});
+
+app.listen(3333, () => console.log("Server is running!"));

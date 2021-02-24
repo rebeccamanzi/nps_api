b326cc0385e4abd2d3b909244aa802a123d57702
Rebecca Manzi <rebeccamanzi@gmail.com>
2021-02-24 19:29:17 -0300

    migration e UserController

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,12 @@
 import 'reflect-metadata';
 import express, { request } from 'express';
 import "./database";
+import { router } from './routes';
+
 const app = express();
 
-/**
- * MÉTODOS HTTP
- * GET => Busca
- * POST => Salvar 
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica (apenas 1 info)
- */
-
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello World - NLW04" });
-});
-
-// 1 param => Rota(Recurso API)
-// 1 param => request, response
-
-app.post("/", (request, response) => {
-  return response.json({ message: "Os dados foram salvos com sucesso!"});
-});
+// use -> middleware
+app.use(express.json()); // permite trabalhar com json (ex: receber no body)
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));

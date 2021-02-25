414f148b4571c4963fc6543998b31242ce6b0342
Rebecca Manzi <rebeccamanzi@gmail.com>
2021-02-25 19:55:02 -0300

    instalando o jest

---
+++ src/__tests__/First.test.ts
@@ -0,0 +1,11 @@
+describe("First", () => {
+  it("deve ser possível somar 2 números", () => {
+    expect(2 + 2).toBe(4)
+  });
+});
+
+describe("First", () => {
+  it("deve ser possível somar 2 números", () => {
+    expect(2 + 2).not.toBe(5)
+  });
+});

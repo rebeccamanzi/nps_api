c18f95913477c32559a1ef2944a00001581a953c
Rebecca Manzi <rebeccamanzi@gmail.com>
2021-02-25 17:39:29 -0300

    listar todas as pesquisas

--- src/controllers/SurveyController.ts
+++ src/controllers/SurveyController.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from "typeorm";
 import { SurveyRepository } from "../repositories/SurveysRepository";
 
 class SurveyController {
+
+  // criar pesquisa
   async create(request: Request, response: Response) {
     const { title, description } = request.body;
 
@@ -17,6 +19,17 @@ class SurveyController {
 
     return response.status(201).json(survey);
   }
+
+  // listar todas as pesquisas
+  async show(request: Request, response: Response) {
+    const surveysRepository = getCustomRepository(SurveyRepository);
+
+    const all = await surveysRepository.find();
+
+    return response.json(all);
+  }
 }
 
 export { SurveyController }
+
+// create() e find() são metodos do typeorm

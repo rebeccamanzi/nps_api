677e6c8c62cd5d5d0bb451ac7145af5c7fde24f3
Rebecca Manzi <rebeccamanzi@gmail.com>
2021-03-15 09:08:28 -0300

    calculo de nps

---
+++ src/controllers/NpsController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import { getCustomRepository, Not, IsNull} from "typeorm";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+
+class NpsController {
+  async execute(request: Request, response: Response) {
+    const { survey_id } = request.params;
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    // buscar respostas de uma determinada pesquisa
+    const surveysUsers = await surveysUsersRepository.find({
+      survey_id,
+      value: Not(IsNull())
+    })
+
+    // filtrar usuários detratores desta pesquisa
+    const detractor = surveysUsers.filter(
+      (survey) => survey.value >= 0 && survey.value <= 6).length;
+
+    // filtrar usuários detratores desta pesquisa
+    const passive = surveysUsers.filter(
+      (survey) => survey.value >= 7 && survey.value <= 8).length;
+
+    // filtrar usuários detratores desta pesquisa
+    const promoters = surveysUsers.filter( 
+      (survey) => survey.value >= 9).length;
+
+    // total de respostas
+    const totalAnswers = surveysUsers.length;
+
+    const calculate = (promoters - detractor) / (totalAnswers) * 100;
+
+    return response.json({
+      detractor,
+      promoters,
+      passive,
+      totalAnswers,
+      nps: calculate
+    })
+  }
+}
+
+export { NpsController };

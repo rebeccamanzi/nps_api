48dc0a062ab870d293715778b1b1a67bbe6374fb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-06 01:55:54 +0800

    Delete src directory

--- src/repositories/SurveysRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Survey } from "../models/Survey";
-
-@EntityRepository(Survey)
-class SurveysRepository extends Repository<Survey> {}
-
-export { SurveysRepository };
--- src/repositories/SurveysUsersRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { SurveyUser } from "../models/SurveyUser";
-
-@EntityRepository(SurveyUser)
-class SurveysUsersRepository extends Repository<SurveyUser> {}
-
-export { SurveysUsersRepository }
